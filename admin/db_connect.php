371bbb4635dbb8842f1cd0b3749dff26fd0c67dc
aKaSteve49 <swastik.hegde2014@gmail.com>
2023-11-17 14:41:12 +0530

    Initial Commit

---
+++ admin/db_connect.php
@@ -0,0 +1,5 @@
+<?php 
+
+$conn= new mysqli('localhost','root','','event_db')or die("Could not connect to mysql".mysqli_error($conn));
+$cconn = mysqli_connect("localhost","root","","event_db");
+?>
